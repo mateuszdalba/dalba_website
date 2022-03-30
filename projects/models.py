9948ee106fb7fc17ae170a1775ea1e25ffbcc50f
mateusz dalba <mateusz.dalba96@gmail.com>
2022-03-30 15:56:43 +0200

    Visual effects update

--- projects/models.py
+++ projects/models.py
@@ -6,7 +6,10 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     technology = models.CharField(max_length=20)
-    image = models.FilePathField(path='/img')
+    image = models.FilePathField(path='projects/static/img/')
+
+    def __str__(self):
+        return self.title
 
 
 
