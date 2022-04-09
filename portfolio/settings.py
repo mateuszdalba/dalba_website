8039f3be7a4b84cce0abaf5a6ab8c13c8d51215c
mateusz dalba <mateusz.dalba96@gmail.com>
2022-04-09 20:20:43 +0200

    Settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -120,14 +120,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+#STATIC_URL = 'static/'
+#STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
-
-
-
-MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#MEDIA_URL = 'media/'
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
